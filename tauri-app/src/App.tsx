5249fd650822021a8382ab4208ec0c0e36dc6a75
sanli <815530704@qq.com>
2024-12-24 13:29:44 +0800

    Initial commit

---
+++ tauri-app/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+import Home from "./pages/Home.tsx";
+
+function App() {
+  return (
+    <Home roomId={"test"}/>
+  );
+}
+
+export default App;
